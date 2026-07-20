8d5b2bf803d39c7ae6f4125f8efd5a7da7488b13
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-07-21 02:37:40 +0500

    feat(calls): CallsConfig - app label calls, BigAutoField default, matching every other app config in the project

---
+++ apps/calls/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CallsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.calls'
+    label = 'calls'
+    verbose_name = 'Call Logs'
