b049c62d5e36041623caa493e691cb970bc19dba
mnavaid925 <158593884+mnavaid925@users.noreply.github.com>
2026-07-22 00:58:19 +0500

    feat(runtime): RuntimeConfig app config (label=runtime, service module)

---
+++ apps/runtime/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class RuntimeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.runtime'
+    label = 'runtime'
+    verbose_name = 'Call Runtime'
